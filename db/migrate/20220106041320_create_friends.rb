cdc0e3bd9dc03b1edd7b1fa13935b3cb037b77f5
tasangpo <tashi.rayshar@gmail.com>
2022-01-05 23:59:02 -0500

    friends table

---
+++ db/migrate/20220106041320_create_friends.rb
@@ -0,0 +1,11 @@
+class CreateFriends < ActiveRecord::Migration[6.1]
+  def change
+    create_table :friends do |t|
+      t.integer :user_id, null: false
+      t.integer :friend_id, null: false
+
+      t.timestamps
+    end
+    add_index :friends, [:user_id, :friend_id], unique: true;
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_30_214428) do
+ActiveRecord::Schema.define(version: 2022_01_06_041320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "friend_id"], name: "index_friends_on_user_id_and_friend_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
