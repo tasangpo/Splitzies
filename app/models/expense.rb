d80782bc7742f2c1384a23d69cc5630e020ce995
tasangpo <tashi.rayshar@gmail.com>
2022-01-31 13:26:39 -0500

    group is now optional in expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -29,6 +29,7 @@ class Expense < ApplicationRecord
     belongs_to :group,
         primary_key: :id,
         foreign_key: :group_id,
-        class_name: :Group
+        class_name: :Group,
+        optional: true
 
 end
